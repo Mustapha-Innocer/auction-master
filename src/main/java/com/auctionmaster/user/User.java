64343ef62cf25d97cd3bf0a197a15b0a2a235746
Mustapha Innocer <mustaphainnocer@gmail.com>
2023-12-15 00:14:02 +0000

    add profile entity relationship in user

--- src/main/java/com/auctionmaster/user/User.java
+++ src/main/java/com/auctionmaster/user/User.java
@@ -6,6 +6,9 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import com.auctionmaster.userprofile.UserProfile;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
@@ -14,6 +17,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
@@ -53,6 +57,12 @@ public class User {
 
 	@Column(name = "is_active", nullable = false)
 	private Boolean isAactive;
+
+	@OneToOne(
+		mappedBy = "user",
+		cascade = CascadeType.ALL
+	)
+	private UserProfile profile;
 
 	@Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	@CreatedDate
