fd406be74ae3d56b6bd2202cf45dacd9014d0c9e
Mustapha Innocer <mustaphainnocer@gmail.com>
2023-12-18 23:21:56 +0000

    Add user repo interface

---
+++ src/main/java/com/auctionmaster/userprofile/UserProfileRepository.java
@@ -0,0 +1,7 @@
+package com.auctionmaster.userprofile;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserProfileRepository extends CrudRepository<UserProfile, Long> {
+	
+}
