f2d853b4d7ec2ebb8cb375e85b4d2c63ba7d6309
Mustapha Innocer <mustaphainnocer@gmail.com>
2023-12-19 00:29:14 +0000

    Add authorities to user type enums

--- src/main/java/com/auctionmaster/user/UserType.java
+++ src/main/java/com/auctionmaster/user/UserType.java
@@ -1,5 +1,17 @@
 package com.auctionmaster.user;
 
+import java.util.Set;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import com.google.common.collect.Sets;
+
 public enum UserType {
-	ADMIN, BIDDER, ARTIST
+	ADMIN,
+	BIDDER,
+	ARTIST;
+
+	public Set<SimpleGrantedAuthority> getAuthorities() {
+		return Sets.newHashSet(new SimpleGrantedAuthority("ROLE_" + this.name()));
+	}
 }
