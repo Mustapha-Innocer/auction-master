2d42d0d8a202b781538ad7859080dd6c099a9dae
Mustapha Innocer <mustaphainnocer@gmail.com>
2024-01-01 20:56:29 +0000

    add authentication controller class

---
+++ src/main/java/com/auctionmaster/auth/AuthController.java
@@ -0,0 +1,24 @@
+package com.auctionmaster.auth;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.auctionmaster.user.User;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@AllArgsConstructor
+public class AuthController {
+
+	private final AuthService authService;
+
+	@PostMapping("/register")
+	public AuthResponse register(@RequestBody User newUser) throws Exception{
+		return authService.register(newUser);
+	}
+	
+}
