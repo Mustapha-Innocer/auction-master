db0124a305451f6751697ed596ad738ff2f52445
Mustapha Innocer <mustaphainnocer@gmail.com>
2024-01-01 20:50:36 +0000

    add authentication user model

---
+++ src/main/java/com/auctionmaster/auth/AuthUser.java
@@ -0,0 +1,56 @@
+package com.auctionmaster.auth;
+
+import java.util.Collection;
+import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class AuthUser implements UserDetails {
+	private final String username;
+	private final String password;
+	private final Set<? extends GrantedAuthority> authorities;
+	private final Boolean isAccountNonExpired;
+	private final Boolean isAccountNonLocked;
+	private final Boolean isCredentialsNonExpired;
+	private final Boolean isEnabled;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		return username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return isAccountNonExpired;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return isAccountNonLocked;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return isCredentialsNonExpired;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return isEnabled;
+	}
+	
+}
