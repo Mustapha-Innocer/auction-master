ed10e20977821931accf5849e9806537205c0c18
Mustapha Innocer <mustaphainnocer@gmail.com>
2024-01-01 20:46:20 +0000

    add appConfig class to conatain app config Beans

---
+++ src/main/java/com/auctionmaster/config/AppConfig.java
@@ -0,0 +1,61 @@
+package com.auctionmaster.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.auctionmaster.auth.AuthUser;
+import com.auctionmaster.user.User;
+import com.auctionmaster.user.UserDAO;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class AppConfig {
+
+	private final UserDAO userDAO;
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(10);
+	}
+
+	@Bean
+	UserDetailsService userDetailsService() {
+		return new UserDetailsService() {
+			@Override
+			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+				User user = userDAO.getUserByEmail(username)
+						.orElseThrow(() -> new UsernameNotFoundException(
+								"%s does not exist.".formatted(username)));
+				return new AuthUser(
+						user.getEmail(),
+						user.getPassword(),
+						user.getRole().getAuthorities(),
+						true, true, true, true);
+			}
+		};
+	}
+
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		return authProvider;
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+		return config.getAuthenticationManager();
+	}
+}
