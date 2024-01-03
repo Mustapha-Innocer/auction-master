cb21e4dc85890caf389b112811c28c30773478a3
Mustapha Innocer <mustaphainnocer@gmail.com>
2024-01-03 15:34:23 +0000

    add logout service

--- src/main/java/com/auctionmaster/config/SecurityConfig.java
+++ src/main/java/com/auctionmaster/config/SecurityConfig.java
@@ -7,8 +7,10 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,23 +19,25 @@ import lombok.RequiredArgsConstructor;
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-	
+
 	private final JwtAuthFilter jwtAuthFilter;
 	private final AuthenticationProvider authenticationProvider;
+	private final LogoutHandler logoutHandler;
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http
-		    .csrf(csrf -> csrf.disable())
-			.authorizeHttpRequests(req -> 
-				req.requestMatchers("/api/v1/auth/*").permitAll()
-				.anyRequest()
-				.authenticated()
-			)
-			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-			.authenticationProvider(authenticationProvider)
-			.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-			.build();
+				.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(req -> req.requestMatchers("/api/v1/auth/*").permitAll()
+						.anyRequest()
+						.authenticated())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authenticationProvider(authenticationProvider)
+				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+				.logout(logout -> logout.logoutUrl("/api/v1/auth/logout")
+						.addLogoutHandler(logoutHandler)
+						.logoutSuccessHandler((req, res, auth) -> SecurityContextHolder.clearContext()))
+				.build();
 	}
 
 }
